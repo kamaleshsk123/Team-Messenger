a8739ff611374927bd4e3a2891b83effb019b74e
Kamalesh <shanmugakamalesh.mhash8.io>
2024-09-27 11:27:21 +0530

    New Messenger Application

---
+++ app/(site)/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import AuthForm from "./components/AuthForm";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full  flex-col justify-center items-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:w-full sm:max-w-md">
+        <Image
+          alt="Logo"
+          height="48"
+          width="48"
+          src="/images/PngItem_5370805.png"
+          className="mx-auto w-auto"
+        />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your Account
+        </h2>
+      </div>
+      <AuthForm />
+    </div>
+  );
+}
