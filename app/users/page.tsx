993ebbaed321e5d54390549e968a41b5d548b9bf
ShanmugaKamalesh M <100531436+kamaleshsk123@users.noreply.github.com>
2024-09-29 20:53:43 +0530

    Added: Sidebar, Navigation and Layout- this will shows the user's side nav for chat, user and logout and last in that tab it shows the user profile

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,11 @@
+import EmptyState from "../components/EmptyState";
+
 const Users = () => {
-  return <div>Hello Team</div>;
+  return (
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </div>
+  );
 };
 
 export default Users;
